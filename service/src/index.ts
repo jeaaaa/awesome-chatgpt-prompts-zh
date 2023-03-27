f8df650f8c9fa87088687e3ae329af12f6feffd0
邓剑 <jea@mail.cc>
2023-03-27 17:49:43 +0800

    node

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,32 +1,32 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import * as csv from 'csv-parser';
-import axios from 'axios';
+import * as fs from "fs";
+import * as path from "path";
+import csv from "csv-parser";
+import axios from "axios";
 
-const ROOT_DIR = path.join(__dirname, '..', '..', '..', '..');
-const PROMPT_FILE = path.join(ROOT_DIR, 'prompt.csv');
-const OUTPUT_FILE = path.join(ROOT_DIR, 'prompt_translated.csv');
+const ROOT_DIR = path.join(__dirname, "..", "..", "..", "..");
+const PROMPT_FILE = path.join(ROOT_DIR, "prompt.csv");
+const OUTPUT_FILE = path.join(ROOT_DIR, "prompt_translated.csv");
 
 const openaiApiKey = process.env.OPENAI_API_KEY;
 
 if (!openaiApiKey) {
-  console.error('OPENAI_API_KEY environment variable not set');
+  console.error("OPENAI_API_KEY environment variable not set");
   process.exit(1);
 }
 
 const translatePrompt = async (prompt: string): Promise<string> => {
   const response = await axios.post(
-    'https://api.openai.com/v1/engines/davinci-codex/completions',
+    "https://api.openai.com/v1/engines/davinci-codex/completions",
     {
       prompt,
       max_tokens: 60,
       n: 1,
-      stop: ['\n'],
+      stop: ["\n"],
       temperature: 0.7,
     },
     {
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         Authorization: `Bearer ${openaiApiKey}`,
       },
     }
@@ -35,7 +35,7 @@ const translatePrompt = async (prompt: string): Promise<string> => {
   const { choices } = response.data?.choices?.[0] ?? {};
 
   if (!choices || choices.length === 0) {
-    throw new Error('Failed to translate prompt');
+    throw new Error("Failed to translate prompt");
   }
 
   return choices[0].text.trim();
@@ -46,7 +46,7 @@ const translatePrompts = async () => {
 
   fs.createReadStream(PROMPT_FILE)
     .pipe(csv())
-    .on('data', async (data) => {
+    .on("data", async (data) => {
       const prompt = data.prompt.trim();
 
       if (prompt) {
@@ -54,8 +54,8 @@ const translatePrompts = async () => {
         prompts.push({ prompt: translatedPrompt });
       }
     })
-    .on('end', () => {
-      fs.writeFileSync(OUTPUT_FILE, '');
+    .on("end", () => {
+      fs.writeFileSync(OUTPUT_FILE, "");
       prompts.forEach((prompt) => {
         fs.appendFileSync(OUTPUT_FILE, `${prompt.prompt}\n`);
       });
